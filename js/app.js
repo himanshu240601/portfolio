a36aea5eb992f524167f1f5113aa1bf9dc06ad6b
unknown <goyalhim69@gmail.com>
2023-01-17 21:25:26 +0530

    minor fixes

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const div = document.getElementById('main-section');
-
-function animate_bg(event){
-    var x = event.clientX;
-    var y = event.clientY;
-
-    div.style.backgroundPosition = `0px ${y*-5/15}px`;
-}
